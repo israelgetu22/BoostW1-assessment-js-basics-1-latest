fa56908614b9db20f6345ddf9ca664a0ec6b28d0
Isarel Getu <israelgetu101@gmail.com>
2022-02-12 23:41:27 -0600

    Problem Solved 5

--- gitInfo.js
+++ gitInfo.js
@@ -50,7 +50,8 @@ console.log(gitCloneDefinition);
 */
 
 //CODE HERE
-
+let gitStatusDefinition = " git status is tell me about this repo";
+console.log(gitStatusDefinition);
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
