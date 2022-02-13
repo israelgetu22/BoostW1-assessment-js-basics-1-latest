6c28b4202977e08e71c5247bf07062affa665a3e
Isarel Getu <israelgetu101@gmail.com>
2022-02-13 00:19:50 -0600

    Problem Solved 4

--- variables.js
+++ variables.js
@@ -24,6 +24,12 @@ let lovesCode = true;
 
 //CODE HERE
 
+if (faveNum === 13) {
+  console.log("lucky 13!");
+} else {
+  console.log(" NOT lucky 13!");
+}
+
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
